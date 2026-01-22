ccc00a6c6c445e95649e549b864e41aeb6215a9b
chuck <choneil@siue.edu>
2026-01-22 11:59:27 -0600

    Changed default activation in conv.py to Hardswish.

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -46,7 +46,8 @@ class Conv(nn.Module):
         default_act (nn.Module): Default activation function (SiLU).
     """
 
-    default_act = nn.SiLU()  # default activation
+    #default_act = nn.SiLU()  # default activation
+    default_act = nn.Hardswish()
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given parameters.
